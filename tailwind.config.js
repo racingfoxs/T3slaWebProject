2a7d0dc8da5b582b2a493ab4b38a6c2c22bbcf5e
DR_GURUNG <racingfoxs@gmail.com>
2023-01-29 17:56:26 +0530

    Pagination implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
+  content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-main":
+          "url('https://www.spacex.com/static/images/backgrounds/dragon_humans.jpg')",
+      },
+      scale: {
+        "-100": "-1",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ]
-}
+  plugins: [require("@tailwindcss/forms")],
+};
