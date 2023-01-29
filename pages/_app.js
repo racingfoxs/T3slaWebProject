a986bbdf4d9c7b4e816504af56e308c9efa21020
DR_GURUNG <racingfoxs@gmail.com>
2023-01-29 14:38:07 +0530

    after Lunch Break Api calls to display data

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,12 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+// https://docs.spacexdata.com/#d65a7f85-e0c7-41ce-b41d-9ad20a238d90
+// https://api.spacexdata.com/v3
+// https://api.spacexdata.com/v3/capsules
+// https://api.spacexdata.com/v3/capsules/{{capsule_serial}}
+// https://api.spacexdata.com/v3/capsules/upcoming
+// https://api.spacexdata.com/v3/capsules/past
+
+
